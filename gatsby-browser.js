3dbc51924a2c58deefe18d0e0de3bb53b9478757
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-10-23 19:27:45 +0200

    :heavy_plus_sign: add dark mode providers to browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import ThemeProvider from "./src/providers/ThemeProvider";
 import "./src/styles/global.css"
+
+export const wrapRootElement = ({ element }) => <ThemeProvider>{element}</ThemeProvider>;
 
 // or:
 // require('./src/styles/global.css')
