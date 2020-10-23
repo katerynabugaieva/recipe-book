e4d4455dcfc2685bf362023651423c1c2f7fdd26
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-10-23 19:28:09 +0200

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, {useContext} from "react"
 import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
+import ToggleTheme from "../components/theme/ToggleTheme"
+import { Wrapper } from "../styles/styles"
+import { ThemeContext } from "../providers/ThemeProvider"
 
 export default function Home({ data }) {
+  const { theme } = useContext(ThemeContext);
+  console.log(
+    "index", theme
+  )
   return (
     <Layout>
+      <Wrapper theme={theme}>
+        <ToggleTheme/>
+      </Wrapper>
       <div>
         <h1
           css={css`
@@ -14,7 +24,7 @@ export default function Home({ data }) {
             border-bottom: 1px solid;
           `}
         >
-          Amazing Pandas Eating Things
+          Хочется вкусненького...
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Рецепты</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
