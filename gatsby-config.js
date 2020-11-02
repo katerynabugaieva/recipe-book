5d4bfa609325ee45c662175e0f65a1caf651ba67
katerynabugaieva <sonnensmile@gmail.com>
2020-11-02 15:34:09 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Список рецептов`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
---
+++ src/pages/eclairs.md
@@ -0,0 +1,9 @@
+---
+title: "Заварные пирожные"
+date: "2017-08-21"
+
+---
+
+ингридиенты для эклеров
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>
