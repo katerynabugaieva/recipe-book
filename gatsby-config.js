7831fa6ff851e5ec5c25bf1dd59467d7d8e0efe3
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-11-03 23:09:30 +0100

    update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Список рецептов`,
+    title: `Хочется вкусненького...`,
   },
   plugins: [
     `gatsby-plugin-emotion`,
