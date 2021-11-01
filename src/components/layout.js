5de50d9eba3454daed3285be467502e36fb3b8b0
katerynabugaieva <sonnensmile@gmail.com>
2021-11-01 20:25:46 +0100

    update theming

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useContext } from "react"
 import { css } from "@emotion/core"
 import { useStaticQuery, Link, graphql } from "gatsby"
 
 import { rhythm } from "../utils/typography"
+import { ThemeContext } from "../providers/ThemeProvider"
+import { Wrapper } from "../styles/styles"
+
 export default function Layout({ children }) {
+  const { theme } = useContext(ThemeContext)
   const data = useStaticQuery(
     graphql`
       query {
@@ -16,25 +20,29 @@ export default function Layout({ children }) {
     `
   )
   return (
-    <div
-      css={css`
-    margin: 0 auto;
-    padding: ${rhythm(2)};
-    padding-top: ${rhythm(1.5)};
-          `}
-    >
-      <Link to={`/`}>
-        <h4
-          css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-          `}
-        >
-          {data.site.siteMetadata.title}
-        </h4>
-      </Link>
-      {children}
-    </div>
+    <Wrapper theme={theme}>
+      <div
+        css={css`
+          margin: 0 auto;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1.5)};
+        `}
+      >
+        <ToggleTheme />
+
+        <Link to={`/`}>
+          <h4
+            css={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            {data.site.siteMetadata.title}
+          </h4>
+        </Link>
+        {children}
+      </div>
+    </Wrapper>
   )
 }
