1e3b2592d502f26313787670910d6440baf3a18b
katerynabugaieva <sonnensmile@gmail.com>
2021-11-03 20:02:12 +0100

    update sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import ToggleTheme from "../components/theme/ToggleTheme"
 import { Wrapper } from "../styles/styles"
 import { ThemeContext } from "../providers/ThemeProvider"
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const Tags = styled.li`
     display: inline;
@@ -14,7 +14,7 @@ const Tags = styled.li`
     color: #555;
     padding: 0px 5px 0px 0px;
   }
-`;
+`
 
 const Divider = styled.div`
    width: 100%;
@@ -22,76 +22,81 @@ const Divider = styled.div`
    opacity: 50%;
    margin: 10px 0px;
   }
-`;
-
+`
+/*
 const Label = styled.label`
    margin: 0px 10px;
-   color: ${({ theme }) => (theme === 'dark' ? 'white' : '#212121')};
+   color: ${({ theme }) => (theme === "dark" ? "white" : "#212121")};
   }
-`;
-
+`*/
 
 export default function Home({ data }) {
-  const { theme } = useContext(ThemeContext);
- 
+  const { theme } = useContext(ThemeContext)
+
   return (
     <Wrapper theme={theme}>
-    <Layout>
-        <ToggleTheme/>
-      <div>
-        <h4> Доступно рецептов: {data.allMarkdownRemark.totalCount}</h4>
-<div style={{marginBottom: "20px"}}>
-  <input type="checkbox"/><Label>Breakfast</Label>
-  <input type="checkbox"/><Label>Without backing</Label>
-  <input type="checkbox"/><Label>Gasts</Label>
-</div>
-
-
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link
-              to={node.fields.slug}
-              css={css`
-                text-decoration: none;
-                color: inherit;
-              `}
-            >
-              <h5
+      <Layout>
+        <ToggleTheme />
+        <div>
+          <h4> Доступно рецептов: {data.allMarkdownRemark.totalCount}</h4>
+          <div style={{ marginBottom: "20px" }}></div>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <Link
+                to={node.fields.slug}
                 css={css`
-                  margin-bottom: ${rhythm(1 / 4)};
+                  text-decoration: none;
+                  color: inherit;
                 `}
               >
-                {node.frontmatter.title}{" "}
-                <span css={css`
-                  color: #555;
-                  font-size: 10px;
-                `}>
-                  — вкус/морочность {node.frontmatter.tasteRating}/{node.frontmatter.effortRating}
-                  
-                </span>
-               <p> 
-                  {node.frontmatter.tags && node.frontmatter.tags.map((tag, index) => (
-          <Tags key={index}>{tag},</Tags>
-        ))}
+                <h5
+                  css={css`
+                    margin-bottom: ${rhythm(1 / 4)};
+                  `}
+                >
+                  {node.frontmatter.title}{" "}
+                  <span
+                    css={css`
+                      color: #555;
+                      font-size: 10px;
+                    `}
+                  >
+                    — вкус/морочность {node.frontmatter.tasteRating}/
+                    {node.frontmatter.effortRating}
+                  </span>
+                  <p>
+                    {node.frontmatter.tags &&
+                      node.frontmatter.tags.map((tag, index) => (
+                        <Tags key={index}>{tag},</Tags>
+                      ))}
+                  </p>
+                </h5>
+                <p
+                  css={css`
+                    margin: 0;
+                  `}
+                >
+                  {node.frontmatter.comment}
                 </p>
-              </h5>
-              <p css={css`
-                margin: 0;
-              `}>{node.frontmatter.comment}</p>
-            </Link>
-            <Divider/>
-          </div>
-        ))}
-      </div>
-    </Layout>
-      </Wrapper>
+              </Link>
+              <Divider />
+            </div>
+          ))}
+        </div>
+      </Layout>
+    </Wrapper>
   )
 }
 
+/**
+  <input type="checkbox"/><Label>Breakfast</Label>
+  <input type="checkbox"/><Label>Without backing</Label>
+  <input type="checkbox"/><Label>Gasts</Label>
+ * 
+ */
 export const query = graphql`
   query {
-    allMarkdownRemark(
-      sort: { order: [DESC, ASC], fields: [frontmatter___tasteRating, frontmatter___effortRating] }){
+    allMarkdownRemark(sort: { fields: frontmatter___title }) {
       totalCount
       edges {
         node {
