e71c80014f119c53df66424cc30a90782c45cdcf
katerynabugaieva <sonnensmile@gmail.com>
2021-11-15 22:17:02 +0100

    update h1 with theming

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,21 +1,22 @@
-import styled from 'styled-components';
-import { rhythm } from '../utils/typography';
+import styled from "styled-components"
 
 export const Wrapper = styled.div`
-margin: 0 auto;
-padding-top: ${rhythm(1.5)};
-  background-color: ${({ theme }) => (theme === 'light' ? 'lavenderblush' : '#000')};
-  
-  h3, h4, p{
-    color: ${({ theme }) => (theme === 'dark' ? '#F0FFFF' : '#212121')};
+  margin: 0 auto;
+  background-color: ${({ theme }) => (theme === "light" ? "#cafafe" : "#000")};
+
+  h1,
+  h2,
+  h3,
+  h4,
+  p {
+    color: ${({ theme }) => (theme === "dark" ? "#F0FFFF" : "#212121")};
   }
 
   h5 {
-    color: ${({ theme }) => (theme === 'dark' ? '#e07b39' : '#1979a9')};
+    color: ${({ theme }) => (theme === "dark" ? "#e07b39" : "#1979a9")};
   }
 
   p {
     font-size: 12px;
   }
- 
-`;
+`
