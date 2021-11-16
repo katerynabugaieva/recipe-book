1a88643e25943280563d39a0a5acc052c520ba42
katerynabugaieva <sonnensmile@gmail.com>
2021-11-16 22:16:26 +0100

    :see no evil: remove console.log

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -1,9 +1,8 @@
+import Layout from "../components/layout"
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
 
 export default function MyFiles({ data }) {
-  console.log(data)
   return (
     <Layout>
       aaa
